a17b0a5262677916c50444d4abe74b223ce2aaa6
nicholaskotzias <nicokotzias@gmail.com>
2025-05-07 20:51:45 -0300

    feat: importar lib para utilização do SoftDelete na model de Nivel

--- app/Models/Nivel.php
+++ app/Models/Nivel.php
@@ -3,9 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Nivel extends Model
 {
+
+    use SoftDeletes;
+    
     protected $table = 'niveis';
     protected $fillable = ['nome'];
 
