8e78c315b3b1e7eff96e02ad4ae64c517bcaeb78
nicholaskotzias <nicokotzias@gmail.com>
2025-05-07 21:09:18 -0300

    feat: adicionar rota para a model Nivel

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\NivelController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('/niveis', NivelController::class);
+
+
