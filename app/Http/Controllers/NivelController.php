5a66a35dcded0f3652cac4ceae57dac869f6735b
nicholaskotzias <nicokotzias@gmail.com>
2025-05-07 21:03:11 -0300

    feat: adicionar index ao controler de Nivel mostrando todos os niveis 'not null'

---
+++ app/Http/Controllers/NivelController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Nivel;
+use Illuminate\Http\Request;
+
+class NivelController extends Controller
+{
+    public function index()
+    {
+        $niveis = Nivel::all();
+        return view('niveis.index')->with(['niveis'=>$niveis]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Nivel $nivel)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Nivel $nivel)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Nivel $nivel)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Nivel $nivel)
+    {
+        //
+    }
+}
