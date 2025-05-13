252b5cdea24c6c21cb7c9b0106fef3347bb2e392
nicholaskotzias <nicokotzias@gmail.com>
2025-05-13 09:23:31 -0300

    feat: adicionar ROTA para a controller de Documento e Categoria

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
+use App\Http\Controllers\CategoriaController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\NivelController;
 use App\Http\Controllers\CursoController;
 use App\Http\Controllers\TurmaController;
+use App\Http\Controllers\DocumentoController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -12,5 +14,8 @@ Route::get('/', function () {
 Route::resource('/niveis', NivelController::class);
 Route::resource('/cursos', CursoController::class);
 Route::resource('/turmas', TurmaController::class);
+Route::resource('/categorias', CategoriaController::class);
+Route::resource('/documentos', DocumentoController::class);
+
 
 
