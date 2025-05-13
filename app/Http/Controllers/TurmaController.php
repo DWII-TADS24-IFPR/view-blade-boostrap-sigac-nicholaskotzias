5484308b667c80fef1c1eee3caa32d3e1a8e74e1
nicholaskotzias <nicokotzias@gmail.com>
2025-05-12 23:37:20 -0300

    feat: adicionar CRUD de Turma, usando validação no 'store' e 'update'

---
+++ app/Http/Controllers/TurmaController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Curso;
+use App\Models\Turma;
+use Illuminate\Http\Request;
+
+class TurmaController extends Controller
+{
+    public function index()
+    {
+        $turmas = Turma::all();
+        return view('turmas.index')->with(['turmas' => $turmas]);
+    }
+
+    public function create()
+    {
+        $cursos = Curso::all();
+        return view('turmas.create')->with('cursos', $cursos);
+    }
+
+    public function store(Request $request)
+    {
+         $request->validate([
+            'ano' => 'required|integer|min:2000',
+            'curso_id' => 'required|exists:cursos,id'
+        ]);
+
+
+        $turma = Turma::create([
+            'ano' => $request->ano,
+            'curso_id' => $request->curso_id,
+        ]);
+
+        return redirect()->route('turmas.index')->with(['success' => 'Turma ' . $turma->ano . ' criado com sucesso!']);
+    }
+
+    public function show(string $id)
+    {
+        $turma = Turma::findOrFail($id);
+        $curso = $turma->curso;
+        return view('turmas.show')->with(['turma' => $turma, 'curso' => $curso]);
+    }
+
+    public function edit(string $id)
+    {
+        $turma = Turma::findOrFail($id);
+        $cursos = Curso::all();
+
+        return view('turmas.edit')->with(['turma' => $turma, 'cursos' => $cursos]);
+    }
+
+    public function update(Request $request, string $id)
+    {
+        $turma = Turma::findOrFail($id);
+
+        if ($request->ano === $turma->ano) {
+            return back()->withErrors(['ano' => 'O ano não foi alterado, tente um ano diferente!'])->withInput();
+        }
+
+       $request->validate([
+            'ano' => 'required|integer|min:2000',
+            'curso_id' => 'required|exists:cursos,id'
+        ]);
+
+        $turma->update([
+            'ano' => $request->ano,
+            'curso_id' => $request->curso_id,
+        ]);
+
+        return redirect()->route('turmas.index')->with(['success' => 'Turma ' . $turma->ano . ' atualizado com sucesso!']);
+    }
+
+    public function destroy(string $id)
+    {
+        $turma = Turma::findOrFail($id);
+        $turma -> delete();
+
+        return redirect()->route('turmas.index')->with(['success' => 'Turma ' . $turma->nome . ' excluido com sucesso!']);
+    }
+}
