479ab2f59af2a10e6c662d8139f83b92a022aeeb
nicholaskotzias <nicokotzias@gmail.com>
2025-05-12 23:37:38 -0300

    feat: adicionar ROTA para a controller de Curso

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\NivelController;
 use App\Http\Controllers\CursoController;
+use App\Http\Controllers\TurmaController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,5 +11,6 @@ Route::get('/', function () {
 
 Route::resource('/niveis', NivelController::class);
 Route::resource('/cursos', CursoController::class);
+Route::resource('/turmas', TurmaController::class);
 
 
