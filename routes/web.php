6f4ee5432124391c04cac65b5e8cb839f24f70a5
nicholaskotzias <nicokotzias@gmail.com>
2025-05-13 23:26:55 -0300

    feat: adicionar ROTAS para o controller de Aluno, Comprovante e Declaracao

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
+use App\Http\Controllers\AlunoController;
 use App\Http\Controllers\CategoriaController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\NivelController;
 use App\Http\Controllers\CursoController;
 use App\Http\Controllers\TurmaController;
 use App\Http\Controllers\DocumentoController;
+use App\Http\Controllers\ComprovanteController;
+use App\Http\Controllers\DeclaracaoController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -16,6 +19,9 @@ Route::resource('/cursos', CursoController::class);
 Route::resource('/turmas', TurmaController::class);
 Route::resource('/categorias', CategoriaController::class);
 Route::resource('/documentos', DocumentoController::class);
+Route::resource('/alunos', AlunoController::class);
+Route::resource('/comprovantes', ComprovanteController::class);
+Route::resource('/declaracoes', DeclaracaoController::class);
 
 
 
